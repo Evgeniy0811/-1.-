840b0d4b304fab89c5b63262b3e3b4806bebe679
pros-2007@mail.ru <pros-2007@mail.ru>
2022-01-25 14:42:42 +0300

    Inicial commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+
+public class SQRService {
+
+    public int sqrCalculate(int lower, int upper) {
+        int number = 0;
+        for (int i = 10; i <= 99; i++) {
+                if (i * i >= lower && i * i <= upper) {
+                    number++;
+                }
+            }
+            System.out.println(number);
+            return number;
+        }
+
+    }
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SQRServiceTest {
+    @Test
+
+    // Диапазон за пределами значений квадратов
+    void shouldCalculate1() {
+        SQRService service = new SQRService();
+        int lower = 40;
+        int upper = 90;
+        int expected = 0;
+        int actual = service.sqrCalculate(lower, upper);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+        // Диапазон в середине значений квадратов
+    void shouldCalculate2() {
+        SQRService service = new SQRService();
+        int lower = 350;
+        int upper = 450;
+        int expected = 3;
+        int actual = service.sqrCalculate(lower, upper);
+        assertEquals(expected, actual);
+    }
+    @Test
+        // Граничные значения диапазона
+    void shouldCalculate3() {
+        SQRService service = new SQRService();
+        int lower = 100;
+        int upper = 9801;
+        int expected = 90;
+        int actual = service.sqrCalculate(lower, upper);
+        assertEquals(expected, actual);
+    }
+    @Test
+        // Одинаковое значение нижней и верхней границ диапазона
+    void shouldCalculate4() {
+        SQRService service = new SQRService();
+        int lower = 500;
+        int upper = 500;
+        int expected = 0;
+        int actual = service.sqrCalculate(lower, upper);
+        assertEquals(expected, actual);
+    }
+}
